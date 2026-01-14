79b6169039c6e6fc834e449c2585d119dc6ffafc
shameel804 <shameeabid2@gmail.com>
2026-01-15 02:18:45 +0530

    feat: Add Django and SQL quiz data and update constants.

--- Constants.dart
+++ Constants.dart
@@ -1,12 +1,21 @@
 class Constants {
   static const List<AllLanguages> allLanguages = [
-    AllLanguages(name: 'Python', apiName: 'python'),
+    AllLanguages(name: 'Python', apiName: 'python', tier: LangTier.free),
+    AllLanguages(name: 'Django', apiName: 'django', tier: LangTier.vip),
+    AllLanguages(name: 'SQL', apiName: 'sql', tier: LangTier.vip),
   ];
 }
+
+enum LangTier { free, vip }
 
 class AllLanguages {
   final String name;
   final String apiName;
+  final LangTier tier;
 
-  const AllLanguages({required this.name, required this.apiName});
+  const AllLanguages({
+    required this.name,
+    required this.apiName,
+    required this.tier,
+  });
 }
